28be8d5b95b94c8437a98d622e2f9e89beb766d8
Sergey Borozenniy <Borozenniy.sergey@gmail.com>
2023-01-19 17:52:07 +0100

    first upd

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+   return (
+      <>
+         <title>Linktree</title>
+         <meta name="description" content="Next.js + Taiwind CSS" />
+         <link rel="icon" href="/favicon.icon" />
+         <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </>
+   );
+}
